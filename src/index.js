7bc95c9509e2d1219a257bd2a37bf01ab0c03400
Jacob Buck <buck.jacob@gmail.com>
2018-03-11 15:35:11 +1300

    Move files to src directory

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import arrify from 'arrify';
+import curry from 'curry';
+
+const propsChanged = (propNames, objectA, objectB) => {
+  if (objectA === objectB) {
+    return false;
+  }
+
+  if (
+    typeof objectA !== 'object' ||
+    objectA === null ||
+    typeof objectB !== 'object' ||
+    objectB === null
+  ) {
+    return true;
+  }
+
+  return arrify(propNames).some(
+    propName => objectA[propName] !== objectB[propName]
+  );
+};
+
+export default curry(propsChanged);
