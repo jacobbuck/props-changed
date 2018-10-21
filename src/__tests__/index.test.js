71c6943896abc3417ce4176f415c05b7a6d8a458
Jacob Buck <buck.jacob@gmail.com>
2018-10-22 10:29:16 +1300

    Arrow function tests

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,27 +1,27 @@
 import propsChanged from '..';
 
-test('compares objects', function() {
+test('compares objects', () => {
   expect(propsChanged(['x'], { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(false);
   expect(propsChanged(['x', 'y'], { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(true);
 });
 
-test('compares objects with propNames passed as string', function() {
+test('compares objects with propNames passed as string', () => {
   expect(propsChanged('x', { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(false);
   expect(propsChanged('y', { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(true);
 });
 
-test('compares objects without defined properties', function() {
+test('compares objects without defined properties', () => {
   expect(propsChanged(['z'], { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(false);
   expect(propsChanged(['z'], { z: 1, y: 1 }, { x: 1, y: 2 })).toBe(true);
   expect(propsChanged(['z'], { x: 1, y: 1 }, { z: 1, y: 2 })).toBe(true);
   expect(propsChanged(['y', 'z'], { x: 1, y: 1 }, { x: 1, y: 2 })).toBe(true);
 });
 
-test('compares objects with strict equality comparison', function() {
+test('compares objects with strict equality comparison', () => {
   expect(propsChanged(['x'], { x: 1 }, { x: '1' })).toBe(true);
 });
 
-test('compares the same object', function() {
+test('compares the same object', () => {
   const obj = { x: 1 };
   expect(propsChanged(['x'], obj, obj)).toBe(false);
   expect(propsChanged(['x', 'y', 'z'], obj, obj)).toBe(false);
