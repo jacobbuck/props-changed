7aed223362254c92f7f32a9dbf87fa7e9d9a1951
Jacob Buck <buck.jacob@gmail.com>
2021-04-20 05:09:59 +0000

    Added ES Module build

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: {
-    exports: 'default',
-    file: 'lib/index.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      exports: 'default',
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
   external: ['curry'],
   plugins: [babel({ babelHelpers: 'bundled' })],
 };
