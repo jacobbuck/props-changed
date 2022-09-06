3ebaa44fa3bee29670283eac7f15a7318eeb6edf
Jacob Buck <buck.jacob@gmail.com>
2022-09-06 05:49:41 +0000

    Remove curry dependency

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,8 @@ import pkg from './package.json';
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'cjs', sourcemap: true, exports: 'default' },
+    { file: pkg.main, format: 'cjs', sourcemap: true },
     { file: pkg.module, format: 'esm', sourcemap: true },
   ],
-  external: ['curry'],
   plugins: [babel({ babelHelpers: 'bundled' })],
 };
