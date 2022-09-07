b7232068b9cb990d45345a2971fe4dbb1f2a3998
Jacob Buck <buck.jacob@gmail.com>
2022-09-07 09:51:12 +0000

    Refactor utils

--- src/utils.js
+++ src/utils.js
@@ -6,16 +6,19 @@ export const curry = (fn) => {
   return curried;
 };
 
+export const isObject = (obj) => !!obj && typeof obj === 'object';
+
 export const path = (path, obj) =>
   (typeof path === 'string' ? path.split('.') : path).reduce(
     (result, prop) =>
-      result && Object.hasOwn(result, prop) ? result[prop] : undefined,
+      isObject(result) && Object.hasOwn(result, prop)
+        ? result[prop]
+        : undefined,
     obj
   );
 
 export const pathsEqWith = (comparitor, paths, objA, objB) =>
-  !objA || !objB || typeof objA !== 'object' || typeof objB !== 'object'
-    ? false
-    : objA === objB
-    ? true
-    : paths.every((p) => comparitor(path(p, objA), path(p, objB)));
+  isObject(objA) &&
+  isObject(objB) &&
+  (objA === objB ||
+    paths.every((p) => comparitor(path(p, objA), path(p, objB))));
