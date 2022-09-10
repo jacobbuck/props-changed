f762725804bc96064ead4cfe8739ba6a2d7af4df
Jacob Buck <buck.jacob@gmail.com>
2022-09-10 04:41:54 +0000

    More refactoring

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import { curry, path } from './utils';
 
+export const propChangedWith = curry(
+  (comparitor, prop, objA, objB) =>
+    objA !== objB && !comparitor(path(prop, objA), path(prop, objB))
+);
+
 export const propsChangedWith = curry(
   (comparitor, props, objA, objB) =>
     objA !== objB &&
-    props.some((p) => !comparitor(path(p, objA), path(p, objB)))
-);
-
-export const propChangedWith = curry((comparator, prop, objA, objB) =>
-  propsChangedWith(comparator, [prop], objA, objB)
+    props.some((prop) => !comparitor(path(prop, objA), path(prop, objB)))
 );
 
 export const propChanged = propChangedWith(Object.is);
