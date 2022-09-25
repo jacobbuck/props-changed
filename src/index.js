78db99316919d94426ee2c3e98dada593e9e2d15
Jacob Buck <buck.jacob@gmail.com>
2022-09-25 07:40:39 +0000

    Optimisations Druplicating code to reduce sice when gzipped or tree shaking

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ export const propsEqual = curry((props, objA, objB) =>
   props.every((prop) => Object.is(path(prop, objA), path(prop, objB)))
 );
 
-export const propsChanged = curry(
-  (props, objA, objB) => !propsEqual(props, objA, objB)
+export const propsChanged = curry((props, objA, objB) =>
+  props.some((prop) => !Object.is(path(prop, objA), path(prop, objB)))
 );
