c034e548ddeb3a90b465106f72bbaaaeafac3efe
Jacob Buck <buck.jacob@gmail.com>
2022-09-25 07:40:55 +0000

    Order exports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { curry, path } from './utils';
 
-export const propsEqual = curry((props, objA, objB) =>
-  props.every((prop) => Object.is(path(prop, objA), path(prop, objB)))
-);
-
 export const propsChanged = curry((props, objA, objB) =>
   props.some((prop) => !Object.is(path(prop, objA), path(prop, objB)))
+);
+
+export const propsEqual = curry((props, objA, objB) =>
+  props.every((prop) => Object.is(path(prop, objA), path(prop, objB)))
 );
